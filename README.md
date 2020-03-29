eb81e0021c8199bc3cff6ebd5283b3a1cbe7a276
BrhmDnsK263 <denysiuk.isi@gmail.com>
2020-03-29 09:30:37 -0300

    script and docs

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# filesgrouper
--
+# FilesGrouper
+
+Moves files in groups into folders.
+
+Utility for moving files in groups into folders with a max number of files in each group.
+
+Useful for dividing large groups of files that slow down some file managers moving them into subfolders.
+
+
+## Usage
+
+For help:
+```bash
+filesgrouper.py [-h/--help]
+```
+To group files in the current directory
+```bash
+filesgrouper.py
+```
+To group files in the specified directory
+```bash
+filesgrouper.py [path]
+```
+To group files in the current directory in group of specified number of files
+```bash
+filesgrouper.py [number]
+```
+to group files in the specified directory in group of specified number of files
+```bash
+filesgrouper.py [path] [number]
+```
+
+
+## Usage from another script
+
+```python
+from filesgrouper import filesGroup
+
+filesGroup(
+		path,
+		quantity_group,
+		folder_base_name,
+		folder_counter,
+		zerofill
+		):
+```
+
+
+Keyword arguments:
+```
+path -- directory of the files to be moved.
+quantity_group -- number of the max number of files in each group.
+folder_base_name -- prefix for the name of the folders will be used.
+folder_counter -- starting number to name each folder after prefix.
+zerofill -- integer of how many zeros to fill the folder_counter.
+```
+
+## Built With
+
+* Python3
---
+++ filesgrouper.py
@@ -0,0 +1,131 @@
+import os
+from sys import argv, exit
+
+"""Moves files in groups into folders.
+
+Useful for dividing large numbers of files in the same directory
+moving them into subfolders.
+
+"filesgrouper.py [-h/--help]" for help.
+
+"filesgrouper.py" to group files in the current directory
+
+"filesgrouper.py [path]" to group files in the specified directory
+
+"filesgrouper.py [number]" to group files in the current directory
+                           in group of specified number of files
+
+"filesgrouper.py [path] [number]" to group files in the specified directory
+                                  in group of specified number of files
+"""
+
+
+#default config
+default_path="."
+default_quantity_group = 10
+default_folder_base_name = "zzfolder"
+default_folder_counter = 0
+default_zerofill = 5
+
+#help
+help_message ="""
+For help use "-h" or "--help"
+
+"filesgrouper.py" to group files in the current directory
+
+"filesgrouper.py [path]" to group files in the specified directory
+
+"filesgrouper.py [number]" to group files in the current directory
+                           in group of specified number of files
+
+"filesgrouper.py [path] [number]" to group files in the specified directory
+                                  in group of specified number of files
+"""
+
+
+def filesGroup(
+		path=default_path,
+		quantity_group=default_quantity_group,
+		folder_base_name=default_folder_base_name,
+		folder_counter=default_folder_counter,
+		zerofill=default_zerofill
+		):
+	"""Moves files in groups into folders.
+
+	Keyword arguments:
+	path -- directory of the files to be moved
+	quantity_group -- number of the max number of files in each group
+	folder_base_name -- prefix for the name of the folders will be used
+	folder_counter -- starting number to name each folder after prefix
+	zerofill -- integer of how many zeros to fill the folder_counter
+	"""
+	files = [file for file in os.listdir(path) if (
+																								os.path.isfile(
+																										os.path.join(
+																												path,
+																												file
+																												)
+																										)
+																								and file != __file__)]
+
+	for index, item in enumerate(files):
+		if(index%quantity_group == 0):			
+			folder_name = folder_base_name + str(folder_counter).zfill(zerofill)
+			new_folder_path = os.path.join(path,folder_name)
+			folder_counter += 1
+			
+			if not os.path.exists(new_folder_path):
+				print("creating folder: {}".format(folder_name))
+				os.mkdir(new_folder_path)
+			else:
+				print("using existing folder: {}".format(folder_name))
+
+		print("Moving file: {} to {}".format(item,new_folder_path))
+		os.rename(os.path.join(path, item), os.path.join(new_folder_path, item))
+		print("{} moved".format(item))
+
+	print("Finish...")
+
+
+if __name__ == "__main__":
+
+	if len(argv) == 1:
+		filesGroup()
+		exit()
+
+	if len(argv) > 1:
+		for arg in argv:
+			if arg == "-h" or arg == "--help":
+				print(help_message)
+				exit()
+
+	if len(argv) == 2:
+		if os.path.isdir(argv[1]):
+			default_path = argv[1]
+		else:
+			try:
+				 default_quantity_group = int(argv[1])
+				 
+			except:
+				print(help_message)
+				exit()
+
+	if len(argv) == 3:
+		if os.path.isdir(argv[1]):
+			default_path = argv[1]
+		else:
+			print(help_message)
+			exit()			
+		try:
+				 default_quantity_group = int(argv[2])				 
+		except:
+			print(help_message)
+			exit()
+
+	filesGroup(
+			path=default_path,
+			quantity_group=default_quantity_group,
+			folder_base_name=default_folder_base_name,
+			folder_counter=default_contador_carpeta,
+			zerofill=default_zerofill
+			)
